3afac3a32c72c7196a649d46c65c970accb8ea13
W.S.S. Perera <40666122+shashperera@users.noreply.github.com>
2025-07-10 09:12:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,13 @@
-#select all databases
-# import pyodbc
-
-# conn = pyodbc.connect(
-#     'DRIVER={ODBC Driver 17 for SQL Server};'
-#     'SERVER=physiotherapy-db.c9ma26yckcjb.eu-north-1.rds.amazonaws.com;'
-#     'UID=admin;'
-#     'PWD=shashipraba12'
-# )
-# cursor = conn.cursor()
-# cursor.execute("SELECT name FROM sys.databases")
-# print(cursor.fetchall())
-# conn.close()
-
 # fetch and print your table data
 import pyodbc
 
 conn = pyodbc.connect(
      'DRIVER={ODBC Driver 17 for SQL Server};'
-    'SERVER=physiotherapy-db.c9ma26yckcjb.eu-north-1.rds.amazonaws.com;'
+    'SERVER=physiotherapy-db.*****.eu-north-1.rds.amazonaws.com;'
         'DATABASE=physiotherapy-db;'
 
     'UID=admin;'
-    'PWD=shashipraba12'
+    'PWD=******'
 )
 cursor = conn.cursor()
 cursor.execute("SELECT * FROM exercises")
